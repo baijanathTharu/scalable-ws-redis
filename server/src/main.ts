3baf4ccb4574e7e43de5a5d4a3d79b3eeadb6089
baijanathTharu <bnchaudhary.tharu@gmail.com>
2024-06-13 22:30:57 +0545

    init

---
+++ server/src/main.ts
@@ -0,0 +1,34 @@
+import { config } from "dotenv";
+
+config({
+  path: ".env",
+});
+
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const appName = process.env.APP_NAME || "";
+const whiteListed = (process.env.WHITELISTED_ORIGINS || "").split(",");
+const PORT = Number(process.env.PORT || 4000);
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: whiteListed,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`a user connected to ${appName}`);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server listening at PORT: ${PORT}`);
+});
